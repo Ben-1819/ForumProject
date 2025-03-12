1b60803998a2195d0592a6f9a65379065016c956
Ben-1819 <ben.brown20332@gmail.com>
2025-03-12 20:45:16 +0000

    feat: Added navigation buttons to the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,32 @@
             </div>
         </div>
     </div>
+
+    <div class="py-12">
+        <a href="{{route("user.index")}}">
+            <button class="rounded-md border-2 border-solid border-red-500">
+                All Users
+            </button>
+        </a>
+        <a href="{{route("friend.requests")}}">
+            <button class="rounded-md border-2 border-solid border-red-500">
+                Your Friend Requests
+            </button>
+        </a>
+        <a href="{{route("friend.index")}}">
+            <button class="rounded-md border-2 border-solid border-red-500">
+                Your Friends
+            </button>
+        </a>
+        <a href="{{route("friend.favourites")}}">
+            <button class="rounded-md border-2 border-solid border-red-500">
+                Your Favourite Friends
+            </button>
+        </a>
+        <a href="{{route("post.create")}}">
+            <button class="rounded-md border-2 border-solid border-red-500">
+                Create A Post
+            </button>
+        </a>
+    </div>
 </x-app-layout>
