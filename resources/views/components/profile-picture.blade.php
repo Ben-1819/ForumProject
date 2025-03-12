f3f7b711262d1d473dd86aa0752185b5911844b6
Ben-1819 <ben.brown20332@gmail.com>
2025-03-12 11:50:56 +0000

    fix: Fixed bug that caused profile pictures to be zoomed
    Changed the tailwind class on the image from object-none to
    object-scale-down

--- resources/views/components/profile-picture.blade.php
+++ resources/views/components/profile-picture.blade.php
@@ -1,9 +1,9 @@
 @if(auth()->check())
 <a href="{{route("profile.show")}}">
-    <img src="/avatars/{{ Auth::user()->avatar }}" class="h-20 w-20 rounded-full object-none object-[59%_-4px]">
+    <img src="/avatars/{{ Auth::user()->avatar }}" class="h-20 w-20 rounded-full object-scale-down object-[59%_-4px]">
 </a>
 @else
 <a href="{{route("register")}}">
-    <img src="/avatars/userIcon.png" class="h-20 w-20 rounded-full object-none object-[59%_-4px]">
+    <img src="/avatars/userIcon.png" class="h-20 w-20 rounded-full object-scale-down object-[59%_-4px]">
 </a>
 @endif
