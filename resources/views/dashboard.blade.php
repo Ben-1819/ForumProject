f794170eb76a9d13c126ea21b09ccfe192535d47
Ben-1819 <ben.brown20332@gmail.com>
2025-03-16 14:25:54 +0000

    fix: Fixed wrong route name for accessing users chats

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,7 +41,7 @@
                 Create A Post
             </button>
         </a>
-        <a href="{{route("chats")}}">
+        <a href="{{route("user.chats")}}">
             <button class="rounded-md border-2 border-solid border-red-500">
                 Message Other Users
             </button>
